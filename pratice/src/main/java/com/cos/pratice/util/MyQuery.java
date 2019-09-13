0a9c9dc1ad856eca4f02dc74464024c2aa50e2f9
kang in <ken940816@gamil.com>
2019-09-13 21:40:45 +0900

    설명완료

--- pratice/src/main/java/com/cos/pratice/util/MyQuery.java
+++ pratice/src/main/java/com/cos/pratice/util/MyQuery.java
@@ -2,20 +2,14 @@ package com.cos.pratice.util;
 
 public class MyQuery {
 	
-	private String selectall;
-	private String dbsave;
+	private String selectall;	//SQL文を入れるString変数宣言
 	
-	
-	public String getSelectall() {
-		return selectall;
+
+	public String getSelectall() {		//Getter
+		return selectall;					
 	}
-	public void setSelectall(String selectall) {
+	public void setSelectall(String selectall) {	//Setter
 		this.selectall 	= 		selectall;
 	}
-	public String getDbsave() {
-		return dbsave;
-	}
-	public void setDbsave(String dbsave) {
-		this.dbsave 	= 		dbsave;
-	}
+	
 }
